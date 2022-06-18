e8c723b269e3d712cea5241968b85cde3ab5b9a8
Andrew Baxter <asbaxter1@gmail.com>
2022-06-18 10:18:45 -0400

    added functionality to event listeners that knows that element was clicked and passed that information to the quizlogic function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,69 @@
-const questions = [
+let choice = '';
+let questions = [
     {
         question: "what is the airspeed velocity of an unladen swallow?",
         choice1: "Fast",
         choice2: "speedy",
         choice3: "slow",
         choice4: "turtle",
+        correct: 'a'
+    },
+    {
+        question: "how much wood could a woodchuck chuck",
+        choice1: "a lot",
+        choice2: "a little",
+        choice3: "none",
+        choice4: "infinity",
+        correct: 'a'
     }
 ];
 
-const question = "hello world";
-
-
-
 function displayQuestions() {
 
-    const questionEl = document.querySelector('#questionText');
-    
-    questionEl.textContent = question;
 
-    
+    let questionEl = document.querySelector('#questionText');
+    questionEl.textContent = questions[0].question;
 
+    let choiceEl1 = document.querySelector('#choice1');
+    let choiceEl2 = document.querySelector('#choice2');
+    let choiceEl3 = document.querySelector('#choice3');
+    let choiceEl4 = document.querySelector('#choice4');
+   
+    choiceEl1.textContent = questions[0].choice1;
+    choiceEl2.textContent = questions[0].choice2;
+    choiceEl3.textContent = questions[0].choice3;
+    choiceEl4.textContent = questions[0].choice4;
 
-    
-
+    choiceEl1.addEventListener("click", e => {
+        let choice = 'a';
+        quizLogic(choice);
+    })
+    choiceEl2.addEventListener("click", e => {
+        let choice = 'b';
+        quizLogic(choice);
+    })
+    choiceEl3.addEventListener("click", e => {
+        let choice = 'c';
+        quizLogic(choice);
+    })
+    choiceEl4.addEventListener("click", e => {
+        let choice = 'd';
+        quizLogic(choice);
+    })
 };
 
- displayQuestions(); 
+
+function quizLogic(choice){
+    
+    console.log(choice);
+    if(choice === questions[0].correct){
+        console.log("you chose right")
+    }
+    else {
+        console.log("you are wrong")
+    }
+
+}
 
 
+displayQuestions();
