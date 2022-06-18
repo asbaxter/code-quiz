7fc39d9eb3b0eda409a313ab00e9afe99357d5ff
Andrew Baxter <asbaxter1@gmail.com>
2022-06-17 22:15:50 -0400

    IDK i just worked for hours and made no progress javascript is hard

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+        question: "what is the airspeed velocity of an unladen swallow?",
+        choice1: "Fast",
+        choice2: "speedy",
+        choice3: "slow",
+        choice4: "turtle",
+    }
+];
+
+const question = "hello world";
+
+
+
+function displayQuestions() {
+
+    const questionEl = document.querySelector('#questionText');
+    
+    questionEl.textContent = question;
+
+    
+
+
+    
+
+};
+
+ displayQuestions(); 
+
+
